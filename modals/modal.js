b11442d810286b115e864f46febbf6748b518f41
Tyler Muir <tylermuir42@gmail.com>
2026-02-24 16:13:07 -0700

    Started modals

---
+++ modals/modal.js
@@ -0,0 +1,22 @@
+const openButton = document.querySelector("#open-modal");
+const closeButton = document.querySelector(".close-button");
+const modalEl = document.querySelector("#modal");
+
+function openModal(){
+    modalEl.classList.add("open");
+    modalEl.setAttribute("aria-hidden", "false");
+}
+
+function closeModal(){
+    modalEl.classList.remove("open");
+    modalEl.setAttribute("aria-hidden", "true");
+}
+
+openButton.addEventListener("click", openModal);
+closeButton.addEventListener("click", closeModal);
+
+window.addEventListener("click", (e) => {
+    if (e.target === modalEl) {
+        closeModal();
+    }
+});
