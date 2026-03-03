635c0cd475dd0df50a14bbf2660b8e8241a27171
Tyler Muir <tylermuir42@gmail.com>
2026-03-03 15:16:06 -0700

    Started parameters assignment

---
+++ params/params.js
@@ -0,0 +1,6 @@
+// params.js
+const products = [
+    { id: 1, name: "Product 1", price: 3, image: "https://placehold.co/300" },
+    { id: 2, name: "Product 2", price: 5, image: "https://placehold.co/300" },
+    { id: 3, name: "Product 3", price: 1, image: "https://placehold.co/300" }
+  ];
